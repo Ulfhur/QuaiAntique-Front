999440ff894ef44126e627682b41d8fa56cbc140
Ulfhur <kevmunoz03@gmail.com>
2025-10-18 19:43:28 +0200

    Add "La carte" page

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,7 +2,10 @@ import Route from "./route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),];
+    new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/galerie", "Galerie", "/pages/galerie.html"),
+    new Route("/carte", "La carte", "/pages/carte.html"),
+];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Quai Antique";
